574e365072b1cd99a4697ebc1ec84e8ce980ef9e
Carlos Mario Muriel <cmmurielo@gmail.com>
2022-09-05 22:57:38 -0500

    Logica Usuarios

--- vista/html/usuario/editarClave.php
+++ vista/html/usuario/editarClave.php
@@ -6,17 +6,19 @@ $mysqli = include(ROOT_PATH . "db.php");
 print_r($_POST);
 
 
-// $usuario = strtolower($_POST['usuario']);
-// $contrasena = $_POST['contrasena'];
+$usuario = $_POST['usuario'];
+$contrasena = $_POST['contrasena'];
 
-// $hash = password_hash($contrasena, PASSWORD_DEFAULT);
+$hash = password_hash($contrasena, PASSWORD_DEFAULT);
 
-// $mysqli->query("UPDATE usuarios 
-//                 SET 
-//                 contrasena = '$hash',
-//                 WHERE usuario = '$usuario'");
+echo $hash;
+print_r($hash);
+$mysqli->query("UPDATE usuarios 
+                SET 
+                contrasena = '$hash',
+                WHERE usuario = '$usuario'");
 
-// $host  = $_SERVER['HTTP_HOST'];
-// $extra = 'index.php?accion=usuarios';
-// header("Location: http://$host/inventool-php/$extra");
-// exit();
+$host  = $_SERVER['HTTP_HOST'];
+$extra = 'index.php?accion=usuarios';
+header("Location: http://$host/inventool-php/$extra");
+exit();
