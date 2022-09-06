b2cc0cc4a563ceec76c2905d061f601f45aba646
Carlos Mario Muriel <cmmurielo@gmail.com>
2022-09-05 22:18:05 -0500

    Logica Usuarios

--- vista/html/usuario/eliminarUsuario.php
+++ vista/html/usuario/eliminarUsuario.php
@@ -6,5 +6,10 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/inventool-php/dirs.php');
 $mysqli = include(ROOT_PATH . "db.php");
 $resultado = $mysqli->query("DELETE FROM usuarios WHERE usuario = '$usuario' ");
 
-header("Location: http://localhost/inventool-php/index.php?accion=usuarios");
+
+$host  = $_SERVER['HTTP_HOST'];
+$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+$extra = 'index.php?accion=usuarios';
+header("Location: http://$host$uri/$extra");
+
 exit();
