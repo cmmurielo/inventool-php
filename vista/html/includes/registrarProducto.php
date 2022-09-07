7391545282049f2e5a4a966764030002bd7f4004
Carlos Mario Muriel <cmmurielo@gmail.com>
2022-09-06 22:16:55 -0500

    Logica Productos

--- vista/html/includes/registrarProducto.php
+++ vista/html/includes/registrarProducto.php
@@ -3,34 +3,28 @@
 include_once($_SERVER['DOCUMENT_ROOT'] . '/inventool-php/dirs.php');
 $mysqli = include(ROOT_PATH . "db.php");
 
-print_r($_POST);
-
 $directorio_imagen = FILES_PATH . 'productos/';
-echo $directorio_imagen;
 $nombre_imagen = $_FILES['imagen']['name'];
-echo $nombre_imagen;
 $tipo_imagen = $_FILES['imagen']['type'];
-echo $tipo_imagen;
 $tamagno_imagen = $_FILES['imagen']['size'];
-echo $tamagno_imagen;
 $imagen_tmp = $_FILES['imagen']['tmp_name'];
 move_uploaded_file($imagen_tmp, $directorio_imagen . $nombre_imagen);
 
-// $producto_codigo = (int)$_POST['producto_codigo'];
-// $nombre = strtoupper($_POST['nombre']);
-// $descripcion = $_POST['descripcion'];
-// $costo = (int)($_POST['costo']);
-// $saldoBodega = (int)$_POST['saldoBodega'];
-// $cantidadMinima = (int)$_POST['cantidadMinima'];
-// $cantidadMaxima = (int)$_POST['cantidadMaxima'];
-// $proveedor_id = (int)$_POST['proveedor_id'];
-// $categoria_id = (int)$_POST['categoria_id'];
+$producto_codigo = (int)$_POST['producto_codigo'];
+$nombre = strtoupper($_POST['nombre']);
+$descripcion = $_POST['descripcion'];
+$costo = (int)($_POST['costo']);
+$saldoBodega = (int)$_POST['saldoBodega'];
+$cantidadMinima = (int)$_POST['cantidadMinima'];
+$cantidadMaxima = (int)$_POST['cantidadMaxima'];
+$proveedor_id = (int)$_POST['proveedor_id'];
+$categoria_id = (int)$_POST['categoria_id'];
 
-// $mysqli->query("INSERT INTO producto_proveedor (producto_codigo, proveedor_documento)
-//                 VALUES ('$producto_codigo','$proveedor_id'");
+$mysqli->query("INSERT INTO producto_proveedor (producto_codigo, proveedor_documento)
+                VALUES ('$producto_codigo','$proveedor_id'");
 
-// $mysqli->query("INSERT INTO productos (producto_codigo, nombre, descripcion, costo, saldoBodega, cantidadMinima, cantidadMaxima, categoria_id, imagen)
-//                 VALUES ('$producto_codigo','$nombre', '$descripcion', '$costo','$saldoBodega','$cantidadMinima','$cantidadMaxima','$categoria_id' , '$nombre_imagen')");
+$mysqli->query("INSERT INTO productos (producto_codigo, nombre, descripcion, costo, saldoBodega, cantidadMinima, cantidadMaxima, categoria_id, imagen)
+                VALUES ('$producto_codigo','$nombre', '$descripcion', '$costo','$saldoBodega','$cantidadMinima','$cantidadMaxima','$categoria_id' , '$nombre_imagen')");
 
 // $host  = $_SERVER['HTTP_HOST'];
 // $extra = 'index.php?accion=productos';
