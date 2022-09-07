ff1ce071875058e60575bb85a452f5f35451bbee
Carlos Mario Muriel <cmmurielo@gmail.com>
2022-09-06 22:05:21 -0500

    Logica Productos

--- vista/html/includes/registrarFactura.php
+++ vista/html/includes/registrarFactura.php
@@ -9,24 +9,26 @@ $documento = (int)$_POST['documento'];
 $usuario = $_POST['usuario'];
 $valor = (int)$_POST['valor_netoFactura'];
 
-$mysqli->query("INSERT INTO facturas (factura_id,cliente_documento,usuarios_usuario,valor)
-                VALUES ('$idFactura','$documento', '$usuario', '$valor')");
+print_r($_POST);
 
-$detalles = json_decode($_POST['detalles']);
+// $mysqli->query("INSERT INTO facturas (factura_id,cliente_documento,usuarios_usuario,valor)
+//                 VALUES ('$idFactura','$documento', '$usuario', '$valor')");
 
-foreach ($detalles as $detalle) {
-    $saldoBodega = $mysqli->query("SELECT saldoBodega from productos WHERE producto_codigo = '$detalle[0]'");
-    $rowSaldoBodega = $saldoBodega->fetch_all(MYSQLI_ASSOC);
-    $nuevoSaldoBodega = $rowSaldoBodega[0]['saldoBodega'] - $detalle[2];
+// $detalles = json_decode($_POST['detalles']);
 
-    $mysqli->query("INSERT INTO detalle_factura (factura_id,producto_codigo,cantidad,descuento,valor)
-                VALUES ('$idFactura','$detalle[0]', '$detalle[2]', '$detalle[5]', '$detalle[7]')");
+// foreach ($detalles as $detalle) {
+//     $saldoBodega = $mysqli->query("SELECT saldoBodega from productos WHERE producto_codigo = '$detalle[0]'");
+//     $rowSaldoBodega = $saldoBodega->fetch_all(MYSQLI_ASSOC);
+//     $nuevoSaldoBodega = $rowSaldoBodega[0]['saldoBodega'] - $detalle[2];
 
-    $mysqli->query("UPDATE productos
-            SET saldoBodega = '$nuevoSaldoBodega'
-            WHERE producto_codigo = '$detalle[0]'");
-}
+//     $mysqli->query("INSERT INTO detalle_factura (factura_id,producto_codigo,cantidad,descuento,valor)
+//                 VALUES ('$idFactura','$detalle[0]', '$detalle[2]', '$detalle[5]', '$detalle[7]')");
 
-$host  = $_SERVER['HTTP_HOST'];
-$extra = 'index.php?accion=ventas';
-header("Location: http://$host/inventool-php/$extra");
+//     $mysqli->query("UPDATE productos
+//             SET saldoBodega = '$nuevoSaldoBodega'
+//             WHERE producto_codigo = '$detalle[0]'");
+// }
+
+// $host  = $_SERVER['HTTP_HOST'];
+// $extra = 'index.php?accion=ventas';
+// header("Location: http://$host/inventool-php/$extra");
