23b1465e4bc1303a219418db6a634bc47b7136c1
Carlos Mario Muriel <cmmurielo@gmail.com>
2022-09-06 22:10:11 -0500

    Logica Productos

--- vista/html/includes/registrarFactura.php
+++ vista/html/includes/registrarFactura.php
@@ -1,35 +1,32 @@
 <?php
 
-// include_once($_SERVER['DOCUMENT_ROOT'] . '/inventool-php/dirs.php');
-// $mysqli = include(ROOT_PATH . "db.php");
+include_once($_SERVER['DOCUMENT_ROOT'] . '/inventool-php/dirs.php');
+$mysqli = include(ROOT_PATH . "db.php");
 
-// $idFactura = (int)$_POST['idFactura'];
-// $fecha = $_POST['fecha'];
-// $documento = (int)$_POST['documento'];
-// $usuario = $_POST['usuario'];
-// $valor = (int)$_POST['valor_netoFactura'];
-echo 'Hola';
+$idFactura = (int)$_POST['idFactura'];
+$fecha = $_POST['fecha'];
+$documento = (int)$_POST['documento'];
+$usuario = $_POST['usuario'];
+$valor = (int)$_POST['valor_netoFactura'];
 
-print_r($_POST);
+$mysqli->query("INSERT INTO facturas (factura_id,cliente_documento,usuarios_usuario,valor)
+                VALUES ('$idFactura','$documento', '$usuario', '$valor')");
 
-// $mysqli->query("INSERT INTO facturas (factura_id,cliente_documento,usuarios_usuario,valor)
-//                 VALUES ('$idFactura','$documento', '$usuario', '$valor')");
+$detalles = json_decode($_POST['detalles']);
 
-// $detalles = json_decode($_POST['detalles']);
+foreach ($detalles as $detalle) {
+    $saldoBodega = $mysqli->query("SELECT saldoBodega from productos WHERE producto_codigo = '$detalle[0]'");
+    $rowSaldoBodega = $saldoBodega->fetch_all(MYSQLI_ASSOC);
+    $nuevoSaldoBodega = $rowSaldoBodega[0]['saldoBodega'] - $detalle[2];
 
-// foreach ($detalles as $detalle) {
-//     $saldoBodega = $mysqli->query("SELECT saldoBodega from productos WHERE producto_codigo = '$detalle[0]'");
-//     $rowSaldoBodega = $saldoBodega->fetch_all(MYSQLI_ASSOC);
-//     $nuevoSaldoBodega = $rowSaldoBodega[0]['saldoBodega'] - $detalle[2];
+    $mysqli->query("INSERT INTO detalle_factura (factura_id,producto_codigo,cantidad,descuento,valor)
+                VALUES ('$idFactura','$detalle[0]', '$detalle[2]', '$detalle[5]', '$detalle[7]')");
 
-//     $mysqli->query("INSERT INTO detalle_factura (factura_id,producto_codigo,cantidad,descuento,valor)
-//                 VALUES ('$idFactura','$detalle[0]', '$detalle[2]', '$detalle[5]', '$detalle[7]')");
+    $mysqli->query("UPDATE productos
+            SET saldoBodega = '$nuevoSaldoBodega'
+            WHERE producto_codigo = '$detalle[0]'");
+}
 
-//     $mysqli->query("UPDATE productos
-//             SET saldoBodega = '$nuevoSaldoBodega'
-//             WHERE producto_codigo = '$detalle[0]'");
-// }
-
-// $host  = $_SERVER['HTTP_HOST'];
-// $extra = 'index.php?accion=ventas';
-// header("Location: http://$host/inventool-php/$extra");
+$host  = $_SERVER['HTTP_HOST'];
+$extra = 'index.php?accion=ventas';
+header("Location: http://$host/inventool-php/$extra");
--- vista/html/includes/registrarProducto.php
+++ vista/html/includes/registrarProducto.php
@@ -3,29 +3,31 @@
 include_once($_SERVER['DOCUMENT_ROOT'] . '/inventool-php/dirs.php');
 $mysqli = include(ROOT_PATH . "db.php");
 
-$directorio_imagen = FILES_PATH . 'productos/';
-$nombre_imagen = $_FILES['imagen']['name'];
-$tipo_imagen = $_FILES['imagen']['type'];
-$tamagno_imagen = $_FILES['imagen']['size'];
-$imagen_tmp = $_FILES['imagen']['tmp_name'];
-move_uploaded_file($imagen_tmp, $directorio_imagen . $nombre_imagen);
+print_r($_POST);
 
-$producto_codigo = (int)$_POST['producto_codigo'];
-$nombre = strtoupper($_POST['nombre']);
-$descripcion = $_POST['descripcion'];
-$costo = (int)($_POST['costo']);
-$saldoBodega = (int)$_POST['saldoBodega'];
-$cantidadMinima = (int)$_POST['cantidadMinima'];
-$cantidadMaxima = (int)$_POST['cantidadMaxima'];
-$proveedor_id = (int)$_POST['proveedor_id'];
-$categoria_id = (int)$_POST['categoria_id'];
+// $directorio_imagen = FILES_PATH . 'productos/';
+// $nombre_imagen = $_FILES['imagen']['name'];
+// $tipo_imagen = $_FILES['imagen']['type'];
+// $tamagno_imagen = $_FILES['imagen']['size'];
+// $imagen_tmp = $_FILES['imagen']['tmp_name'];
+// move_uploaded_file($imagen_tmp, $directorio_imagen . $nombre_imagen);
 
-$mysqli->query("INSERT INTO producto_proveedor (producto_codigo, proveedor_documento)
-                VALUES ('$producto_codigo','$proveedor_id'");
+// $producto_codigo = (int)$_POST['producto_codigo'];
+// $nombre = strtoupper($_POST['nombre']);
+// $descripcion = $_POST['descripcion'];
+// $costo = (int)($_POST['costo']);
+// $saldoBodega = (int)$_POST['saldoBodega'];
+// $cantidadMinima = (int)$_POST['cantidadMinima'];
+// $cantidadMaxima = (int)$_POST['cantidadMaxima'];
+// $proveedor_id = (int)$_POST['proveedor_id'];
+// $categoria_id = (int)$_POST['categoria_id'];
 
-$mysqli->query("INSERT INTO productos (producto_codigo, nombre, descripcion, costo, saldoBodega, cantidadMinima, cantidadMaxima, categoria_id, imagen)
-                VALUES ('$producto_codigo','$nombre', '$descripcion', '$costo','$saldoBodega','$cantidadMinima','$cantidadMaxima','$categoria_id' , '$nombre_imagen')");
+// $mysqli->query("INSERT INTO producto_proveedor (producto_codigo, proveedor_documento)
+//                 VALUES ('$producto_codigo','$proveedor_id'");
 
-$host  = $_SERVER['HTTP_HOST'];
-$extra = 'index.php?accion=productos';
-header("Location: http://$host/inventool-php/$extra");
+// $mysqli->query("INSERT INTO productos (producto_codigo, nombre, descripcion, costo, saldoBodega, cantidadMinima, cantidadMaxima, categoria_id, imagen)
+//                 VALUES ('$producto_codigo','$nombre', '$descripcion', '$costo','$saldoBodega','$cantidadMinima','$cantidadMaxima','$categoria_id' , '$nombre_imagen')");
+
+// $host  = $_SERVER['HTTP_HOST'];
+// $extra = 'index.php?accion=productos';
+// header("Location: http://$host/inventool-php/$extra");
